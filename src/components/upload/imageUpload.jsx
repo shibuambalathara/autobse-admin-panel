89b2e11ca53000d71a7234c3925dfc67db25f228
Amjad Ali <Amjath@autobse.com>
2024-11-04 10:42:01 +0530

    corrected the current error for join image so made an array

--- src/components/upload/imageUpload.jsx
+++ src/components/upload/imageUpload.jsx
@@ -19,7 +19,7 @@ function ImageUpload() {
         type="file"
         className="w-fit py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none"
         onChange={handleChange}
-        accept="/image/*"
+        accept="image/*"
         multiple
       />
       {files.length > 0 && (
@@ -36,13 +36,15 @@ function ImageUpload() {
           <p>{percent}% done</p>
         </>
       )}
+      {console.log(downloadUrls)
+      }
       {downloadUrls.length > 0 && (
         <>
           <p>URLs:</p>
           <div>
             <textarea
               className="w-full border-2 border-black rounded-md h-36 p-2"
-              value={downloadUrls.join("\n")}
+              value={ downloadUrls.join("\n") }
               readOnly
             />
           </div>
