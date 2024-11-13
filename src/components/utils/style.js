b747de6129e54b1ee8f5c1bb7d4cc6588a875e41
Amjad Ali <Amjath@autobse.com>
2024-11-13 16:37:10 +0530

    new feature bid modal for vehicles for bid  api

--- src/components/utils/style.js
+++ src/components/utils/style.js
@@ -21,6 +21,6 @@ export const textAreaStyle = { data: `border border-black p-2 rounded-md w-full 
 export const ButtonStylePagination={data:'px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600'}
 export const ButtonStyleDisabledPagination={data:' px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600'} 
 export const pageNumber = { data: "mx-1 border-2 border-blue-500 rounded-lg p-2 text-blue-600" };
-export const Tablebutton = { data: "text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out min-w-12" };
+export const Tablebutton = { data: "text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out" };
 export const submit = { data: "text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out bg-blue-500 hover:bg-blue-600" };
 export const filter = { div: "flex flex-col  w-64",h2:"font-semibold mb-1 text-sm  text-start" };
