4f7e9b316734251976d3791b55d7baaba8437d22
libinautobse <libin@autobse.com>
2024-12-23 11:12:56 +0530

    fix: state name underscore issue solved in auction allowed states.

--- src/components/user/userDetailsComponent.jsx
+++ src/components/user/userDetailsComponent.jsx
@@ -323,7 +323,7 @@ const UserDetailsComponent = () => {
             label="Mobile"
             register={register("mobile", {
               onChange: (e) => {
-                e.target.value = e.target.value.replace(/[^0-9]/g,"")
+                e.target.value = e.target.value.replace(/[^0-9]/g, "")
                 if (e.target.value.length > 10) {
                   e.target.value = e.target.value.slice(0, 10)
                 }
@@ -410,33 +410,33 @@ const UserDetailsComponent = () => {
               Auction Allowed States <span className="text-red-600">*</span>
             </label>
 
-<Controller
-  name="states"
-  control={control}
-  defaultValue={data?.user?.states.map((state) => ({
-    label: state.name,
-    value: state.id,
-  }))}
-  rules={{
-    validate: (value) => value && value.length > 0 || "At least one state must be selected",
-  }}
-  render={({ field }) => (
-    <Select
-      {...field}
-      placeholder={!isEditable?"":"select..."}
-      // required={true}
-      isDisabled={!isEditable}
-      className="border border-black rounded-md w-full"
-      options={allStates?.data?.States?.map((state) => ({
-        label: state.name,
-        value: state.id,
-      }))}
-      isMulti
-      getOptionValue={(option) => option.value}
-      getOptionLabel={(option) => option.label}
-    />
-  )}
-/>
+            <Controller
+              name="states"
+              control={control}
+              defaultValue={data?.user?.states.map((state) => ({
+                label: state.name,
+                value: state.id,
+              }))}
+              rules={{
+                validate: (value) => value && value.length > 0 || "At least one state must be selected",
+              }}
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  placeholder={!isEditable ? "" : "select..."}
+                  // required={true}
+                  isDisabled={!isEditable}
+                  className="border border-black rounded-md w-full"
+                  options={allStates?.data?.States?.map((state) => ({
+                    label: state.name.split('_').join(' '),
+                    value: state.id,
+                  }))}
+                  isMulti
+                  getOptionValue={(option) => option.value}
+                  getOptionLabel={(option) => option.label}
+                />
+              )}
+            />
 
             <p className="text-red-500">
               {errors.states && <span>{errors.states.message}</span>}
